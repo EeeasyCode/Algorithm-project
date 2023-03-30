f1df664948b4b101830d116ab08b68193ec282fb
EeeasyCode <eeeasycode@gmail.com>
2023-03-30 19:21:08 +0900

    [FEAT] tools 생성

--- sort/bubble-sort.py
+++ sort/bubble-sort.py
@@ -1,12 +1,7 @@
-import random
+import tools
 
 # random 값이 20개 들어있는 배열 생성
-rand_arr = []
-for _ in range(20):
-    rand_int = random.randrange(1, 100)
-    while rand_int in rand_arr:
-        rand_int = random.randrange(1, 100)
-    rand_arr.append(rand_int)
+rand_arr = tools.make_random_arr(20)
 
 print("정렬되기 전 배열: ", rand_arr)
 
@@ -16,7 +11,6 @@ def bubble_sort(arr_data: list) -> list:
         for j in range(1, len(arr_data)-i):
             if arr_data[j] < arr_data[j-1]:
                 arr_data[j], arr_data[j-1] = arr_data[j-1], arr_data[j]
-            print(i,'번 round의 ', j, '번째: ', arr_data)
     
     return arr_data
 
--- sort/quick-sort.py
+++ sort/quick-sort.py
@@ -1,11 +1,5 @@
-import random
+import tools
 
 # random 값이 20개 들어있는 배열 생성
-rand_arr = []
-for _ in range(20):
-    rand_int = random.randrange(1, 100)
-    while rand_int in rand_arr:
-        rand_int = random.randrange(1, 100)
-    rand_arr.append(rand_int)
+rand_arr = tools.make_random_arr(20)
 
-print("정렬되기 전 배열: ", rand_arr)
