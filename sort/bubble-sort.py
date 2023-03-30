eafdb5f32c8d6ba274b0c9ccb2f1d25bd597b5ed
EeeasyCode <eeeasycode@gmail.com>
2023-03-30 18:56:17 +0900

    [FEAT] bubble sort solved

--- sort/bubble-sort.py
+++ sort/bubble-sort.py
@@ -9,3 +9,15 @@ for _ in range(20):
     rand_arr.append(rand_int)
 
 print("정렬되기 전 배열: ", rand_arr)
+
+def bubble_sort(arr_data: list) -> list:
+    for i in range(len(arr_data)):
+        for j in range(1, len(arr_data)-i):
+            if arr_data[j] < arr_data[j-1]:
+                arr_data[j], arr_data[j-1] = arr_data[j-1], arr_data[j]
+            print(i,'번 round의 ', j, '번째: ', arr_data)
+    
+    return arr_data
+
+sorted_arr = bubble_sort(rand_arr)
+print(sorted_arr)
