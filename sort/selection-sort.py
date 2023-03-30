ca2329c347c0c607e0621b33e15e2bd571a571e5
EeeasyCode <eeeasycode@gmail.com>
2023-03-30 16:50:09 +0900

    [FEAT] solved

--- sort/selection-sort.py
+++ sort/selection-sort.py
@@ -1,20 +1,28 @@
-import numpy as np
+import random
 
 # random 값이 20개 들어있는 배열 생성
-# rand_arr = np.random.randint(1, 100, size = 20)
-rand_arr = [8, 31, 48, 73, 3, 65, 20, 29, 11, 15]
+rand_arr = []
+for _ in range(20):
+    rand_int = random.randrange(1, 100)
+    while rand_int in rand_arr:
+        rand_int = random.randrange(1, 100)
+    rand_arr.append(rand_int)
 
+print("정렬되기 전 배열: ", rand_arr)
+
+#선택 정렬 알고리즘
 def selection_sort(arr_data: list) -> list:
-    for i in range(len(arr_data)):
+    for i in range(len(arr_data)-1):
         max_index = 0
-        for j in range(len(arr_data)-i):
+        for j in range(1, len(arr_data)-i):
             if arr_data[max_index] < arr_data[j]:
                 max_index = j
-        print(i, "번째 ", arr_data)        
         arr_data[max_index], arr_data[j] = arr_data[j], arr_data[max_index]
     return arr_data
 
-print(selection_sort(rand_arr))
+sorted_arr = selection_sort(rand_arr)
+
+print("정렬 후 배열: ", sorted_arr)
 
 
 
