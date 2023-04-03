83a6f015efa54546b686b8b13a16ebb8c750c14e
EeeasyCode <eeeasycode@gmail.com>
2023-04-03 17:38:50 +0900

    [FEAT] merge sort solved

--- sort/merge-sort.py
+++ sort/merge-sort.py
@@ -3,3 +3,39 @@ import tools
 # random 값이 20개 들어있는 배열 생성
 rand_arr = tools.make_random_arr(20)
 
+def merge_sort(arr: list):
+    def sort(low:int, high:int):
+        if high - low < 2:
+            return
+        mid = (low + high) // 2
+        sort(low, mid)
+        sort(mid, high)
+        merge(low, mid, high)
+
+    def merge(low:int, mid:int, high:int):
+        temp = []
+        l, h = low, mid
+
+        while l < mid and h < high:
+            if arr[l] < arr[h]:
+                temp.append(arr[l])
+                l += 1
+            else:
+                temp.append(arr[h])
+                h += 1
+
+        while l < mid:
+            temp.append(arr[l])
+            l += 1
+        while h < high:
+            temp.append(arr[h])
+            h += 1
+
+        for i in range(low, high):
+            arr[i] = temp[i - low]
+
+    return sort(0, len(arr))    
+
+
+merge_sort(rand_arr)
+print(rand_arr)
