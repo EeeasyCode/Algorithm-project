388f4993b9c675a098ed9747df847bd5a0c3085a
EeeasyCode <eeeasycode@gmail.com>
2023-05-20 13:46:49 +0900

    [SOLVED] dict

--- hash/find.py
+++ hash/find.py
@@ -1,15 +1,31 @@
-n = int(input())
-n_num = list(map(int, input().split()))
+# n = int(input())
+# n_num = list(map(int, input().split()))
 
-h = [0] * n
-for i in n_num:
-    h[i % n] = i
+# h = [0] * n
+# for i in n_num:
+#     h[i % n] = i
+
+# m = int(input())
+# m_num = list(map(int, input().split()))
+
+# for i in m_num:
+#     if (h[i % m] in m_num):
+#         print(1)
+#     else:
+#         print(0)
+
+# dict 사용
+from collections import Counter
+
+n = int(input())
+arr1 = map(int, input().split())
+arr1 = Counter(arr1)
 
 m = int(input())
-m_num = list(map(int, input().split()))
+arr2 = map(int, input().split())
 
-for i in m_num:
-    if (h[i % m] in m_num):
-        print(1)
+for num in arr2:
+    if num in arr1:
+        print('1')
     else:
-        print(0)
+        print('0')
